1ba98633b94a1e2a1d1012be9fe3f49b4d017906
ik012982i9 <slmn.momi@gmail.com>
2023-02-13 09:26:24 +0100

    Arreglado inicio de sesion.

--- JsAvanzado/Zubirizon - recursos/scripts/script.js
+++ JsAvanzado/Zubirizon - recursos/scripts/script.js
@@ -1,20 +1,42 @@
+function load_pagina(pagina){
+    window.location.href = pagina;
+}
+async function login() {
 
-function btnPressed(){
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    
-    var user = fetch("../json/cuentas.json")
-    
-    .then(Response => Response.json())
-    .then(data => {
-        const users = data.users;
-        const isValid = users.some(user =>{
-            return user.username === username && user.password === password;
-        });
-        if(isValid){
-            window.location.href = "../add_products.html";
-        }else{
-            alert("Incorrect username or password");
+    let usuario = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+
+
+    let cuentas_json = [];
+    await fetch('./json/cuentas.json').then(response => response.json()).then(datos_cuentas => cuentas_json = datos_cuentas);
+
+
+    let usuario_valido = 0;
+
+    for (i = 0; i < cuentas_json.length; i++) {
+        if (usuario == "admin" && password == cuentas_json[i].password) {
+            load_pagina('add_product.html');
+            usuario_valido = 1;
+        } else if (usuario == cuentas_json[i].username && password == cuentas_json[i].password) {
+            load_pagina('list_product.html');
+            document.querySelector("#username").innerHTML = usuario;
+            usuario_valido = 1;
         }
-    })
-};
+    }
+    if (usuario_valido == 0) {
+        alert("Error, Usuario o Contraseña incorrectos");
+    }
+}
+// fetch('./script.js')
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
+//     if(username == username && passwrod == passwrod){
+//         alert("Sesion iniciada")
+//     }else{
+//         alert("Datos incorrectos")
+//     }
+// }
+
+
+
--- JsAvanzado/Zubirizon - recursos/scripts/script3.js
+++
@@ -1,8 +0,0 @@
-const productList = document.getElementById("product-list");
-
-// Add logic to fetch the product list from the server and display it in the productList element
-
-const logoutButton = document.getElementById("logout-button");
-logoutButton.addEventListener("click", function() {
-  // Add logic to log out the user
-});
